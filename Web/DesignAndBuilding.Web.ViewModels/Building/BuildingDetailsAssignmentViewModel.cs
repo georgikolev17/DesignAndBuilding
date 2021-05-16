dec9b7fe99790998267084f3b43b5dafc7a42ae5
georgikolev17 <g.t.kolev1@gmail.com>
2021-05-16 18:27:16 +0300

    Fixed building details bugs

---
+++ Web/DesignAndBuilding.Web.ViewModels/Building/BuildingDetailsAssignmentViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignAndBuilding.Web.ViewModels.Building
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using DesignAndBuilding.Data.Models;
+
+    public class BuildingDetailsAssignmentViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public DesignerType DesignerType { get; set; }
+
+        [Required]
+        public decimal BasePricePerSquareMeter { get; set; }
+    }
+}
---
+++ Web/DesignAndBuilding.Web.ViewModels/Building/BuildingDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignAndBuilding.Web.ViewModels.Building
+{
+    using System.Collections.Generic;
+
+    using DesignAndBuilding.Data.Models;
+
+    public class BuildingDetailsViewModel
+    {
+        public string Name { get; set; }
+
+        public decimal TotalBuildUpArea { get; set; }
+
+        public BuildingType BuildingType { get; set; }
+
+        public IEnumerable<BuildingDetailsAssignmentViewModel> Assignments { get; set; }
+    }
+}
