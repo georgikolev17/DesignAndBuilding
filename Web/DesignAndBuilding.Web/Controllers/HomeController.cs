ae53df2aac1f4700bbb8ce8bad3be095b96cb89c
georgikolev17 <g.t.kolev1@gmail.com>
2021-05-20 17:50:33 +0300

    Add Engineer Types and Engineer Home Page

--- Web/DesignAndBuilding.Web/Controllers/HomeController.cs
+++ Web/DesignAndBuilding.Web/Controllers/HomeController.cs
@@ -1,15 +1,52 @@
 ﻿namespace DesignAndBuilding.Web.Controllers
 {
     using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
 
+    using DesignAndBuilding.Data.Models;
+    using DesignAndBuilding.Services;
     using DesignAndBuilding.Web.ViewModels;
-
+    using DesignAndBuilding.Web.ViewModels.Assignment;
+    using DesignAndBuilding.Web.ViewModels.Building;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : BaseController
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IAssignmentsService assignmentsService;
+
+        public HomeController(UserManager<ApplicationUser> userManager, IAssignmentsService assignmentsService)
         {
+            this.userManager = userManager;
+            this.assignmentsService = assignmentsService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+
+            if (user != null && user.DesignerType != DesignerType.Architect)
+            {
+                var assignments = this.assignmentsService
+                    .GetAllAssignmentsForDesignerType(user.DesignerType)
+                    .Select(x => new BuildingDetailsAssignmentViewModel
+                    {
+                        BasePricePerSquareMeter = x.BasePricePerSquareMeter,
+                        Description = x.Description,
+                        DesignerType = x.DesignerType,
+                        EndDate = x.EndDate,
+                        Id = x.Id,
+                    }).ToList();
+                var engineerAssignmentsViewModel = new EngineerAssignmentsViewModel
+                {
+                    Assignments = assignments,
+                };
+
+                return this.View("EngineerIndex", engineerAssignmentsViewModel);
+            }
+
             return this.View();
         }
 
