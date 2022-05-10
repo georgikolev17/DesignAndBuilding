c7d96f6bf908155f698502a2ba74d877bbe40159
georgikolev17 <g.t.kolev1@gmail.com>
2022-05-10 12:05:50 +0300

    Add register different types of users functionality

---
+++ Data/DesignAndBuilding.Data/Migrations/20220510082134_AddBulstatAndCompanyNameColumns.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesignAndBuilding.Data.Migrations
+{
+    public partial class AddBulstatAndCompanyNameColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bulstat",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bulstat",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyName",
+                table: "AspNetUsers");
+        }
+    }
+}
