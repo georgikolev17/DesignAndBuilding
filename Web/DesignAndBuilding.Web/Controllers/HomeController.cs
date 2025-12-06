5531298100e632aab1123d110edd7aeaa00e24f5
Georgi Kolev <g.t.kolev1@gmail.com>
2025-12-06 23:23:20 +0100

    fixed footer design bugs

--- Web/DesignAndBuilding.Web/Controllers/HomeController.cs
+++ Web/DesignAndBuilding.Web/Controllers/HomeController.cs
@@ -66,6 +66,11 @@
             return this.View();
         }
 
+        public IActionResult Cookies()
+        {
+            return this.View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
