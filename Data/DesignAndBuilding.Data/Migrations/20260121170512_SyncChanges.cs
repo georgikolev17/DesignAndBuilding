47c31d043ac71e3ded21d822be38470f2aaae005
Georgi Kolev <g.t.kolev1@gmail.com>
2026-01-21 18:06:27 +0100

    fix bugs

---
+++ Data/DesignAndBuilding.Data/Migrations/20260121170512_SyncChanges.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesignAndBuilding.Data.Migrations
+{
+    public partial class SyncChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
